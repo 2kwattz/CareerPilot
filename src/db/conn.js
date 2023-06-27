fe148c99a29145ef886569275594eff9bb125cd1
Roshan Bhatia <37065373+2kwattz@users.noreply.github.com>
2023-06-27 22:41:52 +0530

    Updated conn.js for cloud

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,8 +1,9 @@
 // Database connection modules
 const mongoose = require('mongoose'); //Importing Mongoose 
+require('dotenv').config(); // For defining environment variables
 
 
-mongoose.connect("mongodb://localhost:27017/carrierpilot", { useNewUrlParser: true, useUnifiedTopology: true}) //Default Address of Database
+mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.vmilsjg.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true}) //Default Address of Database
 .then(function(){
     console.log("Database connected successfully...\n")
 })
